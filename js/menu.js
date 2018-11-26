83908f6d4295015e99057d75ea8d8e16d639a183
Sean Warman <seanwarman@gmail.com>
2018-11-26 14:58:50 +0000

    some extra syling and a gitignore file

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+// We might have to have some kind of global
+// object that shows which page is currently
+// selected. That way we can have a different
+// style for the currently selected menu item.
