031f77d529d1db5357a09fc89ab4ecc59d5ea32f
Sean Warman <seanwarman@gmail.com>
2019-03-11 15:32:31 +0000

    animated title intro

--- js/hero.js
+++ js/hero.js
@@ -3,10 +3,35 @@ let page = document.getElementsByClassName('plx-page')[0];
 
 const initialPosition = 40;
 
-var animate = () => {
+var landscapePosition = () => {
   landscape.style.backgroundPosition = '0 ' + (((page.scrollTop/20)* -1)+initialPosition) + '%';
 }
-animate();
+landscapePosition();
 page.addEventListener('scroll', () => {
-  animate();
+  landscapePosition();
 });
+
+let letters = document.getElementsByClassName('title-letter');
+
+var animateTitle = (num, attribute, value) => {
+  if(num < letters.length) {
+    letters[num].style[attribute] = value;
+    num++;
+    setTimeout(animateTitle, 40, num, attribute, value);
+  }
+}
+
+window.addEventListener('load', () => {
+
+  animateTitle(0, 'textShadow', '0 0 10px #ffffff');
+  setTimeout(animateTitle, 1000, 0, 'textShadow', '0 0 0 #ffffff');
+  setTimeout(animateTitle, 1700, 0, 'textShadow', '0 0 0 #ffffff00');
+  setTimeout(animateTitle, 2000, 0, 'color', '#ffffff');
+  // setTimeout( () => {
+  //   for(var i = 0, length1 = letters.length; i < length1; i++){
+  //     letters[i].style.color = '#ffffff';
+  //   }
+  // }, 5000);
+  
+});
+
