3f5ef13910d83ff3174440c360d9350fd5145618
Sean Warman <seanwarman@gmail.com>
2019-03-16 15:37:10 +0000

    reformatted all other pages

---
+++ js/donate.js
@@ -0,0 +1,62 @@
+let open = false;
+const innerModal = document.getElementById('modal-dialogue');
+const body = document.getElementsByTagName('body')[0];
+const modal = document.getElementById('modal');
+const button = document.getElementById('donate-button');
+const close = document.getElementsByClassName('close-icon')[0];
+
+console.log(button);
+
+var openModal = function() {
+  if(!open) {
+    modal.style.display = 'flex';
+    body.className = 'modal-open';
+    setTimeout( () => {
+      modal.className = 'fade-in';
+      innerModal.className = 'modal-scroll-view';
+    }, 10);
+  } else {
+    
+    body.className = '';
+    setTimeout( () => {
+      modal.className = '';
+      innerModal.className = '';
+    }, 1);
+    setTimeout( () => {
+      modal.style.display = 'none';
+    }, 500);
+  }
+  open = !open;
+}
+
+var preventClick = function(event) {
+  event.stopPropagation();
+}
+
+button.addEventListener('click', openModal);
+modal.addEventListener('click', openModal);
+innerModal.addEventListener('click', preventClick);
+close.addEventListener('click', openModal);
+
+
+let input = document.getElementById('donation-amt');
+
+paypal.Buttons({
+  createOrder: function(data, actions) {
+      // Set up the transaction
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: input.value
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // Capture the funds from the transaction
+      return actions.order.capture().then(function(details) {
+        // Show a success message to your buyer
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+  }).render('.paypal-buttons');
--- js/global.js
+++ js/global.js
@@ -2,18 +2,39 @@ var plxGroup = document.getElementsByClassName('plx-group');
 var plxPage = document.getElementsByClassName('plx-page')[0];
 var navbar = document.getElementById('navbar');
 
-var adjustViewHeight = () => {
-  plxPage.style.paddingTop = navbar.clientHeight+'px';
-  for(var i = 0, length1 = plxGroup.length; i < length1; i++){
-    plxGroup[i].style.height = window.innerHeight - navbar.clientHeight+'px';
+if(plxGroup && plxPage && navbar) {
+  var adjustViewHeight = () => {
+    plxPage.style.paddingTop = navbar.clientHeight+'px';
+    for(var i = 0, length1 = plxGroup.length; i < length1; i++){
+      plxGroup[i].style.height = window.innerHeight - navbar.clientHeight+'px';
+    }
   }
-}
-adjustViewHeight();
-
-window.addEventListener('resize', () => {
   adjustViewHeight();
-});
+  
+  window.addEventListener('resize', () => {
+    adjustViewHeight();
+  });
+}
 
 window.addEventListener('load', () => {
   document.body.classList.remove('hide-me');
 });
+
+var state = false;
+
+var clickMenu = function() {
+  var menu = document.getElementsByClassName('menu-outer-wrapper')[0];
+  var greyedOut = document.getElementsByClassName('greyed-out')[0];
+  var icon = document.getElementById('menu-icon');
+  if(state === false) {
+    menu.style.left = '0px';
+    greyedOut.style.opacity = '.3';
+    state = true;
+    icon.classList = ['fas fa-angle-double-left'];
+  } else {
+    menu.style.left = '-430px';
+    greyedOut.style.opacity = '0';
+    state = false;
+    icon.classList = ['fas fa-angle-double-right'];
+  }
+}
