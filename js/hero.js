bc103b8a592d4b675511e1fa47dc66682abe7103
Sean Warman <seanwarman@gmail.com>
2019-03-16 14:11:05 +0000

    cleaned up files and minor fixes

--- js/hero.js
+++ js/hero.js
@@ -27,11 +27,6 @@ window.addEventListener('load', () => {
   setTimeout(animateTitle, 1000, 0, 'textShadow', '0 0 0 #ffffff');
   setTimeout(animateTitle, 1700, 0, 'textShadow', '0 0 0 #ffffff00');
   setTimeout(animateTitle, 2000, 0, 'color', '#ffffff');
-  // setTimeout( () => {
-  //   for(var i = 0, length1 = letters.length; i < length1; i++){
-  //     letters[i].style.color = '#ffffff';
-  //   }
-  // }, 5000);
   
 });
 
